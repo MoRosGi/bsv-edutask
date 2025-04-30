d3d34973465d9048766e6f55e70c3ce8228a5454
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2025-04-30 19:21:13 +0200

    Work on fixtures

--- frontend/cypress/e2e/todo_item.cy.js
+++ frontend/cypress/e2e/todo_item.cy.js
@@ -1,11 +1,11 @@
 describe('To do creation, delete and edit',() => {
     // define variables that we need on multiple occasions
     let uid // user id
-    let name // name of the user (firstName + ' ' + lastName)
-    let email // email of the user
+    // let name // name of the user (firstName + ' ' + lastName)
+    // let email // email of the user
     let task_id // task id
-    let task_title // title of the task
-    let task_description // description of the task
+    // let task_title // title of the task
+    // let task_description // description of the task
 
     before(function () {
         // create a fabricated user from fixtures
@@ -18,14 +18,17 @@ describe('To do creation, delete and edit',() => {
                 body: user
             }).then((response) => {
                 uid = response.body._id.$oid
-                name = user.firstName + ' ' + user.lastName
-                email = user.email
+                console.log(typeof(uid))
+                // name = user.firstName + ' ' + user.lastName
+                // email = user.email
             })
         })
 
         // create a fabricated task from fixtures
-        cy.fixture('task.json')
+        return cy.fixture('task_test.json')
         .then((task) => {
+            task.userid = uid
+            console.log(task)
             cy.request({
                 method: 'POST',
                 url: 'http://localhost:5000/tasks/create',
@@ -33,11 +36,21 @@ describe('To do creation, delete and edit',() => {
                 body: task
             }).then((response) => {
                 task_id = response.body._id.$oid
-                task_title = task.title
-                task_description = task.description
+                // task_title = task.title
+                // task_description = task.description
             })
         })
     })
+
+
+    beforeEach(function () {
+        // enter the main main page
+        cy.visit('http://localhost:3000')
+    })
+
+    it('starting out on the landing screen', () => {
+        // make sure the landing page contains a header with "login"
+        cy.get('h1')
+        .should('contain.text', 'Login')
+    })
 } )
-
-
