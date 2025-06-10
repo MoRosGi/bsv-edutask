3b301c806214415f9c016d1b50e8bce4e883b4f8
Annie Gustafsson <annie.v.gustafsson@gmail.com>
2025-06-10 14:21:16 +0200

    Move steps in beforeEach to function, adjust setup of done status via backend, change file name

--- frontend/cypress/e2e/todo_item.cy.js
+++ frontend/cypress/e2e/todo_item.cy.js
@@ -1,68 +1,78 @@
-describe('To do creation, delete and edit',() => {
-  // define variables that we need on multiple occasions
-  let uid // user id
-  let email // email of the user
-  let task_id // task id
-  let task_title // title of the task
+// define variables that we need on multiple occasions
+let uid         // user id
+let email       // email of the user
+let task_id     // task id
+let todo_id     // todo id
+let task_title  // title of the task
 
-  beforeEach(function () {
-    return cy.fixture('user.json').then((user) => {
-      return cy.request({
-        method: 'POST',
-        url: 'http://localhost:5000/users/create',
-        form: true,
-        body: user
-      }).then((response) => {
-        uid = response.body._id.$oid
-        email = user.email
-        return cy.fixture('task_test.json').then((task) => {
-          task.userid = uid
-          task_title = task.title
-          return cy.request({
-            method: 'POST',
-            url: 'http://localhost:5000/tasks/create',
-            form: true,
-            body: task
-          }).then((response) => {
-            task_id = response.body[0]["_id"]["$oid"]
-            console.log("task_id response:", task_id)
-            cy.visit('http://localhost:3000')
+/**
+ * Utility function to access the task page.
+ * Used to refresh page after request to backend.
+ * @params {string} email       email of the created user
+ * @params {string} task_title  title of the created task
+*/
+function accessTask(email, task_title) {
+  cy.visit('http://localhost:3000')
 
-            // detect a div which contains "Email Address", find the input and type
-            cy.contains('div', 'Email Address')
-              .find('input[type=text]')
-              .type(email)
+  // detect a div which contains "Email Address", find the input and type
+  cy.contains('div', 'Email Address')
+    .find('input[type=text]')
+    .type(email)
 
-            // submit the form on this page
-            cy.get('form')
-              .submit()
+  // submit the form on this page
+  cy.get('form')
+    .submit()
 
-            // click the task created
-            cy.contains('div', task_title)
-            .click()
-          })
+  // click the task created
+  cy.contains('div', task_title)
+  .click()
+}
+
+beforeEach(function () {
+  // create a user
+  return cy.fixture('user.json').then((user) => {
+    return cy.request({
+      method: 'POST',
+      url: 'http://localhost:5000/users/create',
+      form: true,
+      body: user
+    }).then((response) => {
+      uid = response.body._id.$oid
+      email = user.email
+
+      // create a task for the user
+      return cy.fixture('task_test.json').then((task) => {
+        task.userid = uid
+        task_title = task.title
+        return cy.request({
+          method: 'POST',
+          url: 'http://localhost:5000/tasks/create',
+          form: true,
+          body: task
+        }).then((response) => {
+          task_id = response.body[0]["_id"]["$oid"]
+          todo_id = response.body[0]["todos"][0]["_id"]["$oid"]
+          console.log("task_id response:", task_id)
+
+          // call accessTask to refresh page
+          accessTask(email, task_title)
         })
       })
     })
   })
+})
 
-  // beforeEach(function () {
-  //   cy.visit('http://localhost:3000')
+afterEach(function () {
+  // clean up by deleting the data created from the database
+  cy.request({
+    method: 'DELETE',
+    url: `http://localhost:5000/users/${uid}`
+  }).then((response) => {
+    cy.log(response.body)
+  })
+})
 
-  //   // detect a div which contains "Email Address", find the input and type
-  //   cy.contains('div', 'Email Address')
-  //     .find('input[type=text]')
-  //     .type(email)
-
-  //   // submit the form on this page
-  //   cy.get('form')
-  //     .submit()
-
-  //   // click the task created
-  //   cy.contains('div', task_title)
-  //   .click()
-  // })
-
+describe('R8UC1',() => {
   it('Add button disabled when description empty', () => {
     cy.contains('Add')
       .should('be.disabled')
@@ -88,35 +98,77 @@ describe('To do creation, delete and edit',() => {
       .eq(1)
       .should('contain', 'Test a todo')
   })
+})
 
-  it('Click unchecked todo checked item', () => {
-    cy.get('ul > li')
-      .filter(':has(span)')
-      .last()
-      .children()
-      .eq(0)
-      .click({ force: true })
-      .should('have.class', 'checked')
-      .parents()
-      .children()
-      .eq(1)
-      .should('have.css', 'text-decoration', 'line-through solid rgb(49, 46, 46)')
+describe('R8UC2',() => {
+  it('Click unchecked todo checked item, done set to true', () => {
+    cy.request({
+      method: 'PUT',
+      url: `http://localhost:5000/todos/byid/${todo_id}`,
+      form: true,
+      body: { data: JSON.stringify({"$set": {"done": false}})
+      }
+    }).then(() => {
+      accessTask(email, task_title)
+
+      cy.get('ul > li')
+        .filter(':has(span)')
+        .last()
+        .children()
+        .as('todoItem')
+        .eq(0)
+        .click({ force: true })
+
+      cy.get('@todoItem')
+        .should('have.class', 'checked')
+        .parents()
+        .children()
+        .eq(1)
+        .should('have.css', 'text-decoration', 'line-through solid rgb(49, 46, 46)')
+
+      cy.request({
+        method: 'GET',
+        url: `http://localhost:5000/todos/byid/${todo_id}`
+      }).its('body.done')
+        .should('eq', true)
+    })
   })
 
-  it('Click checked todo unchecked item', () => {
-    cy.get('ul > li')
-      .filter(':has(span)')
-      .last()
-      .children()
-      .eq(0)
-      .click({ force: true })
-      .should('have.class', 'unchecked')
-      .parents()
-      .children()
-      .eq(1)
-      .should('not.have.css', 'text-decoration', 'line-through solid rgb(49, 46, 46)')
-  })
+  it('Click checked todo unchecked item, done set to false', () => {
+    cy.request({
+      method: 'PUT',
+      url: `http://localhost:5000/todos/byid/${todo_id}`,
+      form: true,
+      body: { data: JSON.stringify({"$set": {"done": true}})
+      }
+    }).then(() => {
+      accessTask(email, task_title)
 
+      cy.get('ul > li')
+        .filter(':has(span)')
+        .last()
+        .children()
+        .as('todoItem')
+        .eq(0)
+        .click({ force: true })
+
+      cy.get('@todoItem')
+        .should('have.class', 'unchecked')
+        .parents()
+        .children()
+        .eq(1)
+        .should('not.have.css', 'text-decoration', 'line-through solid rgb(49, 46, 46)')
+
+      cy.request({
+        method: 'GET',
+        url: `http://localhost:5000/todos/byid/${todo_id}`
+      }).its('body.done')
+        .should('eq', false)
+    })
+  })
+})
+
+describe('R8UC3',() => {
   it('Click x item removed', () => {
     cy.get('ul > li')
       .filter(':has(span)')
@@ -124,21 +176,12 @@ describe('To do creation, delete and edit',() => {
       .children()
       .eq(2)
       .click({ force: true })
+
     cy.get('ul > li')
       .filter(':has(span)')
       .last()
       .children()
       .eq(1)
       .should('not.contain', 'Test a todo')
-  })
-
-  afterEach(function () {
-    // clean up by deleting the data created from the database
-    cy.request({
-      method: 'DELETE',
-      url: `http://localhost:5000/users/${uid}`
-    }).then((response) => {
-      cy.log(response.body)
-    })
   })
 })
